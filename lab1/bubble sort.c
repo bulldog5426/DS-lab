819ffb96d0c0e0d7faf0b1581ca2ca7c44a78428
bulldog5426 <141926358+bulldog5426@users.noreply.github.com>
2023-08-18 10:37:33 +0530

    Create bubble sort.c

---
+++ lab1/bubble sort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original array: ");
+    printArray(arr, size);
+
+    bubbleSort(arr, size);
+
+    printf("Sorted array: ");
+    printArray(arr, size);
+
+    return 0;
+}
